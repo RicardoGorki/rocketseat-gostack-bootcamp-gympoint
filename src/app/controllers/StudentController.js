237ca5a276db5fc030f66e32f773d4a440d287ad
RicardoGorki <ricardo.gorki@gmail.com>
2019-10-24 21:21:56 -0300

    Controller and Auth

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: ' Validations fails' });
+    }
+
     const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
@@ -13,6 +26,37 @@ class StudentController {
     const { id, name, email, age, weight, height } = await Student.create(
       req.body
     );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().required(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: ' Validations fails' });
+    }
+
+    const { id } = req.params;
+
+    const { email } = req.body;
+
+    const student = await Student.findByPk(id);
+
+    const { name, age, weight, height } = await student.update(req.body);
 
     return res.json({
       id,
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
 routes.post('/students', StudentController.store);
-routes.put('/students', StudentController.update);
+routes.put('/students/:id', authMiddleware, StudentController.update);
 
 routes.post('/sessions', SessionController.store);
 

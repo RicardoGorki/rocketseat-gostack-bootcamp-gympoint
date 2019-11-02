1d8ce702fc17adb1dd91fa8932a439db76800467
RicardoGorki <ricardo.gorki@gmail.com>
2019-11-02 16:48:50 -0300

    planscontroller-yup

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Plan from '../models/Plan';
 
 class PlanController {
@@ -10,12 +11,28 @@ class PlanController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { id, title, duration, price } = await Plan.create(req.body);
 
     return res.json({ id, title, duration, price });
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { id } = req.params;
 
     const plans = await Plan.findByPk(id);
